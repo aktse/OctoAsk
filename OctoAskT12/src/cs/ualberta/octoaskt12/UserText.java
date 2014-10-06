1398f5ec19975964d7fa13205849d769bb4c07f3
akt <akt@ualberta.ca>
2014-10-06 10:03:17 -0600

    Finished creating xml format for each row. Implemented time mechanic -> testing required

--- OctoAskT12/src/cs/ualberta/octoaskt12/UserText.java
+++ OctoAskT12/src/cs/ualberta/octoaskt12/UserText.java
@@ -1,14 +1,43 @@
 package cs.ualberta.octoaskt12;
 
+import java.util.GregorianCalendar;
+
 public class UserText {
 
-	String userText;
+	private String userText;
+	private int upvotes = 5;
+	private int answers = 3;
+	private GregorianCalendar dateCreated;
 	
 	public UserText(String string){
 		this.userText = string;
+		this.dateCreated = new GregorianCalendar();
 	}
 	
 	public String getUserText(){
 		return this.userText;
+	}
+	
+	public int incrementVotes(){
+		upvotes++;
+		return upvotes;
+	}
+	
+	public int getVotes(){
+		return upvotes;
+	}
+	
+	public int incrementAnswers(){
+		answers++;
+		return answers;
+	}
+	
+	public int getAnswers(){
+		return answers;
+	}
+	
+	public GregorianCalendar getTime(){
+		
+		return dateCreated;
 	}
 }
--- OctoAskT12/src/cs/ualberta/octoaskt12/adapters/CustomArrayAdapter.java
+++ OctoAskT12/src/cs/ualberta/octoaskt12/adapters/CustomArrayAdapter.java
@@ -1,6 +1,9 @@
 package cs.ualberta.octoaskt12.adapters;
 
+import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 import cs.ualberta.octoaskt12.R;
 import cs.ualberta.octoaskt12.UserText;
@@ -35,8 +38,34 @@ public class CustomArrayAdapter extends ArrayAdapter<UserText>{
 				view = inflater.inflate(R.layout.fragment_question_list_item,null);
 		}
 		
-		TextView item = (TextView) view.findViewById(R.id.list_question_title);
-		item.setText(userText.get(position).getUserText());
+		TextView question = (TextView) view.findViewById(R.id.list_question_title);
+		question.setText(userText.get(position).getUserText());
+		TextView answers = (TextView) view.findViewById(R.id.list_question_answer);
+		answers.setText(userText.get(position).getAnswers() + " answers");
+		
+		TextView time = (TextView) view.findViewById(R.id.list_question_time);
+		GregorianCalendar gc = userText.get(position).getTime();
+		GregorianCalendar now = new GregorianCalendar();
+		long difference = now.getTime().getTime() - gc.getTime().getTime();
+		long timeInSeconds = difference/1000;
+		long timeInMinutes = timeInSeconds/60;
+		long timeInHours = timeInMinutes/60;
+		
+		if (timeInSeconds < 60) {
+			time.setText(timeInSeconds + " seconds ago");
+		} else if (timeInMinutes < 60) {
+			time.setText(timeInMinutes + " minutes ago");
+		} else if (timeInHours < 24) {
+			time.setText(timeInHours + " hours ago");
+		} else {
+			time.setText(DateFormat.getDateInstance().format(gc.getTime()));
+		}
+		System.out.println(difference);
+		System.out.println(timeInSeconds);
+		System.out.println(timeInHours);
+
+		TextView upvotes = (TextView) view.findViewById(R.id.list_question_upvote_caption);
+		upvotes.setText(userText.get(position).getVotes() + " upvotes");
 		
 		return view;
 	}
