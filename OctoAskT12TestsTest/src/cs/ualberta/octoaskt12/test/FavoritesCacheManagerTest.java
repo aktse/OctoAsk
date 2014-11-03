22247e0f8ac33cbd1495c3281d0ae8d299e7af6d
0nebit <chuan4@ualberta.ca>
2014-11-02 20:02:06 -0700

    Chuan/Chris - added Favorites Cache Manager test.

---
+++ OctoAskT12TestsTest/src/cs/ualberta/octoaskt12/test/FavoritesCacheManagerTest.java
@@ -0,0 +1,40 @@
+package cs.ualberta.octoaskt12.test;
+
+import android.content.Context;
+import android.test.ActivityInstrumentationTestCase2;
+import cs.ualberta.octoaskt12.Favorites;
+import cs.ualberta.octoaskt12.FavoritesCacheManager;
+import cs.ualberta.octoaskt12.MainActivity;
+import cs.ualberta.octoaskt12.Question;
+import cs.ualberta.octoaskt12.User;
+
+public class FavoritesCacheManagerTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+	public FavoritesCacheManagerTest() {
+		super(MainActivity.class);
+	}
+	
+	public void testSaveLoadQuestions()
+	{
+		Context context = MainActivity.CallContext();
+
+		User user = new User("Kevin");
+		Favorites fav = new Favorites();
+		
+		for (int i = 0; i < 10; i++)
+		{
+			Question question = new Question("Q " + i, "Body " + i, user);
+			fav.add(question);
+		}
+		
+		FavoritesCacheManager fcm = new FavoritesCacheManager(context);
+		
+		fcm.saveFavorites(fav, user);
+		
+		Favorites fav2 = fcm.loadFavorites();
+		
+		assert(fav2.count() == 10);
+		//assertEquals(qal, qal2);
+		//assertEquals(qal2.getSize(), 0);
+	}
+}
